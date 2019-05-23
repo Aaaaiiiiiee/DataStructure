7c4cafd132fd619bfa400d2a763a46d8878d579f
Aaaaiiiiiee <wjjgjk@naver.com>
2019-05-23 17:04:41 +0900

    BinarySearchTree Updated

--- DataStructure/BinarySearchTree.cpp
+++ DataStructure/BinarySearchTree.cpp
@@ -1,5 +1,6 @@
 #include "BinarySearchTree.h"
-#include <stack>
+//#include <stack>
+#include <queue>
 
 BinarySearchTree::BinarySearchTree()
 	: root(nullptr)
@@ -9,38 +10,56 @@ BinarySearchTree::BinarySearchTree()
 
 BinarySearchTree::~BinarySearchTree()
 {
-	stack<Node*> cur_delete;
-	Node* tmp = root;
-	cur_delete.push(tmp);
-	while (!cur_delete.empty())
+	// postfix delete
+	//stack<Node*> cur_delete;
+	//Node* tmp = root;
+	//cur_delete.push(tmp);
+	//while (!cur_delete.empty())
+	//{
+	//	if (tmp->GetLeftNode() != nullptr)	// 왼쪽을 먼저 쭉 탐색
+	//	{
+	//		cur_delete.push(tmp->GetLeftNode());
+	//		tmp = tmp->GetLeftNode();
+	//	}
+	//	else	// 더이상 왼쪽으로 갈 수 없을때
+	//	{
+	//		delete cur_delete.top();	// 해당 Node를 지우고
+	//		cur_delete.top() = nullptr;
+	//		cur_delete.pop();
+	//
+	//		// Node root를 pop한 경우를 예외처리 해야함
+	//		if (cur_delete.empty())	break;
+	//
+	//		tmp = cur_delete.top();		// 그것의 parent node의 오른쪽 탐색
+	//		if (tmp->GetRightNode() != nullptr)	// 오른쪽 탐색
+	//		{
+	//			cur_delete.push(tmp->GetRightNode());
+	//			tmp = tmp->GetRightNode();
+	//		}
+	//		else	// 오른쪽을 더이상 볼 수 없을 때
+	//		{
+	//			delete cur_delete.top();
+	//			cur_delete.top() = nullptr;
+	//			cur_delete.pop();
+	//			tmp = cur_delete.top();
+	//		}
+	//	}
+	//}
+	
+	// root Node부터 시작하여 height별로 delete시켜줌. 
+	queue<Node*> cur_delete;
+	if (root != nullptr)
 	{
-		if (tmp->GetLeftNode() != nullptr)	// 왼쪽을 먼저 쭉 탐색
+		cur_delete.push(root);
+		while (!cur_delete.empty())
 		{
-			cur_delete.push(tmp->GetLeftNode());
-			tmp = tmp->GetLeftNode();
-		}
-		else	// 더이상 왼쪽으로 갈 수 없을때
-		{
-			delete cur_delete.top();	// 해당 Node를 지우고
-			cur_delete.top() = nullptr;
+			Node* tmp = cur_delete.front();
 			cur_delete.pop();
-
-			// Node root를 pop한 경우를 예외처리 해야함
-			if (cur_delete.empty())	break;
-
-			tmp = cur_delete.top();		// 그것의 parent node의 오른쪽 탐색
-			if (tmp->GetRightNode() != nullptr)	// 오른쪽 탐색
-			{
+			if (tmp->GetLeftNode() != nullptr)
+				cur_delete.push(tmp->GetLeftNode());
+			else if (tmp->GetRightNode() != nullptr)
 				cur_delete.push(tmp->GetRightNode());
-				tmp = tmp->GetRightNode();
-			}
-			else	// 오른쪽을 더이상 볼 수 없을 때
-			{
-				delete cur_delete.top();
-				cur_delete.top() = nullptr;
-				cur_delete.pop();
-				tmp = cur_delete.top();
-			}
+			delete tmp;
 		}
 	}
 }
@@ -68,7 +87,7 @@ bool BinarySearchTree::Push(int key)
 	if (root == nullptr)
 	{
 		root = node_new;
-		return;
+		return true;
 	}
 	
 	Node* tmp = root;
@@ -95,18 +114,20 @@ bool BinarySearchTree::Push(int key)
 	return true;
 }
 
-bool BinarySearchTree::Delete(int key)
-{
-	Node* node_delete = Search(key);
+//bool BinarySearchTree::Delete(int key)
+//{
+//	Node* node_delete = Search(key);
+//
+//	if (node_delete != nullptr)
+//	{
+//		delete node_delete;
+//		return true;
+//	}
+//	else
+//		return false;
+//}
 
-	if (node_delete != nullptr)
-	{
-		delete node_delete;
-		return true;
-	}
-	else
-		return false;
-}
+// child가 0,1,2개 일때 따져서 delete해야함.
 
 bool BinarySearchTree::DownTree(string sequence)
 {
