bf1e6413bd1352e37d2e76609770d09c3567bf49
Aaaaiiiiiee <wjjgjk@naver.com>
2019-06-03 21:19:08 +0900

    BinarySearchTree Backup

--- DataStructure/AVLTree.cpp
+++ DataStructure/AVLTree.cpp
@@ -11,3 +11,13 @@ AVLTree::AVLTree()
 AVLTree::~AVLTree()
 {
 }
+
+bool AVLTree::Insert(int key)
+{
+	return false;
+}
+
+bool AVLTree::Delete(int key)
+{
+	return false;
+}
--- DataStructure/AVLTree.h
+++ DataStructure/AVLTree.h
@@ -6,5 +6,20 @@ class AVLTree : public BinarySearchTree
 public:
 	AVLTree();
 	~AVLTree();
+
+	/*
+	*	AVLTree bool Insert(int)
+	*	Insert 성공 여부 반환
+	*	true : Insert 성공
+	*	false : Insert 실패
+	*/
+	bool Insert(int key);
+	/*
+	*	AVLTree bool Delete(int)
+	*	Delete 성공 여부 반환
+	*	true : Delete 성공
+	*	false : Delete 실패
+	*/
+	bool Delete(int key);
 };
 
